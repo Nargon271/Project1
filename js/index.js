d2a8bc4c7961d313bcfedd76dab979ba4a4b15a5
Gonzalo Arguelles <garguelles2701@gmail.com>
2020-11-01 18:09:28 +0100

    hecho el dom y pantalla de inicio

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-window.onload = () => game.init('canvas')
+window.onload = () => {
+    document.getElementById('start-button').onclick = () => {
+        startGame();
+    }
+}
+
+function startGame() {
+    const divDisplay = document.querySelector('#gameStart')
+    divDisplay.style.display = 'none'
+    game.init('canvas')
+
+}
